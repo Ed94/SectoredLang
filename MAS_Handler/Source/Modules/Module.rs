efabfe65365d77d736d8c75b2b0c91ce90120dba
Ed94 <edwardgz@gmail.com>
2018-12-15 21:19:57 -0500

    Added prototype for use with packaging with the handler later on.
    Added first files. Need to add stuf to the First.pkspec. Deleted old example file (obsolete)

--- MAS_Handler/Source/Modules/Module.rs
+++ MAS_Handler/Source/Modules/Module.rs
@@ -1,8 +1,8 @@
 
 //All the the supported module types.
-#[path = "Metal/Metal.rs"            ] pub mod Metal      ;   //Metal     "Module"
-#[path = "Assembler/Assembler.rs"    ] pub mod Assembler  ;   //Assembler "Module"
-#[path = "Compiler/Compiler.rs"      ] pub mod Compiler   ;   //Compiler  "Module"
+#[path = "Metal/Metal.rs"            ] pub mod Metal      ;   //Metal       "Module"
+#[path = "Assembler/Assembler.rs"    ] pub mod Assembler  ;   //Assembler   "Module"
+#[path = "Compiler/Compiler.rs"      ] pub mod Compiler   ;   //Compiler    "Module"
 #[path = "Interpreter/Interpreter.rs"] pub mod Interpreter;   //Interpreter "Module"
-#[path = "Functional/Functional.rs"  ] pub mod Functional ;   //Functional "Module"
-#[path = "Logical/Logical.rs"        ] pub mod Logical    ;   //Logical "Module"
+#[path = "Functional/Functional.rs"  ] pub mod Functional ;   //Functional  "Module"
+#[path = "Logical/Logical.rs"        ] pub mod Logical    ;   //Logical     "Module"
